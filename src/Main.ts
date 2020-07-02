2c1fd91f97b8e57bac034ecd78757fcda0738987
Robin-Sb <robin.schwab@hs-furtwangen.de>
2020-07-02 14:20:39 +0200

    experimental viewport overlaying

--- src/Main.ts
+++ src/Main.ts
@@ -4,6 +4,7 @@ namespace Platform_Edtior {
 
     fudge.RenderManager.initialize(true, true);
     export let viewport: fudge.Viewport;
+    export let editorViewport: fudge.Viewport;
     window.addEventListener("load", editorLoad);
 
     let oldX: number;
@@ -11,36 +12,46 @@ namespace Platform_Edtior {
 
     function editorLoad(_event: Event): void {
         let graph: fudge.Node = new fudge.Node("graph");
-        const canvas: HTMLCanvasElement = document.querySelector("canvas");
-        
+        const canvas: HTMLCanvasElement = document.querySelector("#scene_canvas");
+        oldX = canvas.width / 2;
+        oldY = canvas.height / 2;
         
         let cmpCamera: fudge.ComponentCamera = new fudge.ComponentCamera();
         cmpCamera.pivot.translateZ(5);
-        cmpCamera.pivot.lookAt(ƒ.Vector3.ZERO());
-        cmpCamera.backgroundColor = ƒ.Color.CSS("LightSkyBlue");
+        cmpCamera.pivot.lookAt(fudge.Vector3.ZERO());
+        cmpCamera.backgroundColor = fudge.Color.CSS("LightSkyBlue");
 
-        fudgeAid.addStandardLightComponents(graph, new ƒ.Color(0.5, 0.5, 0.5));
+        fudgeAid.addStandardLightComponents(graph, new fudge.Color(0.5, 0.5, 0.5));
 
-        let testCube: fudge.Node = new fudgeAid.Node("Test", new fudge.Matrix4x4(), new fudge.Material("TestMtr", fudge.ShaderFlat, new fudge.CoatColored(fudge.Color.CSS("green"))), new fudge.MeshCube); 
+        let testCube: fudge.Node = new fudgeAid.Node("Test", new fudge.Matrix4x4(), new fudge.Material("TestMtr", fudge.ShaderFlat, new fudge.CoatColored(fudge.Color.CSS("green"))), new fudge.MeshQuad); 
         graph.addChild(testCube);
 
         viewport = new fudge.Viewport();
 
         viewport.initialize("Viewport", graph, cmpCamera, canvas);
-        viewport.addEventListener(ƒ.EVENT_POINTER.MOVE, pointerMove);
-        viewport.activatePointerEvent(ƒ.EVENT_POINTER.MOVE, true);
+        viewport.addEventListener(fudge.EVENT_POINTER.MOVE, pointerMove);
+        viewport.activatePointerEvent(fudge.EVENT_POINTER.MOVE, true);
+
+        const editorCanvas: HTMLCanvasElement = document.querySelector("#editor_canvas");
+
+        editorViewport = new fudge.Viewport();
+        let testNode: fudge.Node = new fudgeAid.Node("Test", new fudge.Matrix4x4(), new fudge.Material("TestMtr", fudge.ShaderFlat, new fudge.CoatColored(fudge.Color.CSS("green"))), new fudge.MeshCube);
+        let testCamera: fudge.ComponentCamera = new fudge.ComponentCamera();
+        testCamera.pivot.translateZ(5);
+        testCamera.pivot.lookAt(fudge.Vector3.ZERO());
+        testCamera.backgroundColor = fudge.Color.CSS("White");
+ 
+        editorViewport.initialize("Test", testNode, testCamera, editorCanvas);
+        editorViewport.draw();
 
         viewport.draw();
     }
 
     function pointerMove(_event: fudge.EventPointer): void {
-        let scalar: number = 0.005;
-        if (!oldX || !oldY) {
-            oldX = _event.canvasX;
-            oldY = _event.canvasY;
-        } else if (fudge.Keyboard.isPressedOne([fudge.KEYBOARD_CODE.SHIFT_LEFT])) {
-            let xChange: number = (_event.canvasX - oldX) * scalar;
-            let yChange: number = (_event.canvasY - oldY) * scalar;
+        let scale: number = 0.005;
+        if (fudge.Keyboard.isPressedOne([fudge.KEYBOARD_CODE.SHIFT_LEFT])) {
+            let xChange: number = (_event.canvasX - oldX) * scale;
+            let yChange: number = (_event.canvasY - oldY) * scale;
     
             viewport.camera.pivot.translate(new fudge.Vector3(xChange, yChange, 0));
         }
@@ -48,5 +59,9 @@ namespace Platform_Edtior {
         oldY = _event.canvasY;
         viewport.draw();
     }
-}
 
+    function convertToMainViewport(selectedNode: fudge.Node): void {
+        viewport.getGraph().removeChild(selectedNode);
+        editorViewport.getGraph().addChild(selectedNode);
+    }
+}
--- src/crun.js
+++ src/crun.js
@@ -9,34 +9,45 @@ var Platform_Edtior;
     let oldY;
     function editorLoad(_event) {
         let graph = new fudge.Node("graph");
-        const canvas = document.querySelector("canvas");
+        const canvas = document.querySelector("#scene_canvas");
+        oldX = canvas.width / 2;
+        oldY = canvas.height / 2;
         let cmpCamera = new fudge.ComponentCamera();
         cmpCamera.pivot.translateZ(5);
-        cmpCamera.pivot.lookAt(ƒ.Vector3.ZERO());
-        cmpCamera.backgroundColor = ƒ.Color.CSS("LightSkyBlue");
-        fudgeAid.addStandardLightComponents(graph, new ƒ.Color(0.5, 0.5, 0.5));
-        let testCube = new fudgeAid.Node("Test", new fudge.Matrix4x4(), new fudge.Material("TestMtr", fudge.ShaderFlat, new fudge.CoatColored(fudge.Color.CSS("green"))), new fudge.MeshCube);
+        cmpCamera.pivot.lookAt(fudge.Vector3.ZERO());
+        cmpCamera.backgroundColor = fudge.Color.CSS("LightSkyBlue");
+        fudgeAid.addStandardLightComponents(graph, new fudge.Color(0.5, 0.5, 0.5));
+        let testCube = new fudgeAid.Node("Test", new fudge.Matrix4x4(), new fudge.Material("TestMtr", fudge.ShaderFlat, new fudge.CoatColored(fudge.Color.CSS("green"))), new fudge.MeshQuad);
         graph.addChild(testCube);
         Platform_Edtior.viewport = new fudge.Viewport();
         Platform_Edtior.viewport.initialize("Viewport", graph, cmpCamera, canvas);
         Platform_Edtior.viewport.addEventListener("\u0192pointermove" /* MOVE */, pointerMove);
         Platform_Edtior.viewport.activatePointerEvent("\u0192pointermove" /* MOVE */, true);
+        const editorCanvas = document.querySelector("#editor_canvas");
+        Platform_Edtior.editorViewport = new fudge.Viewport();
+        let testNode = new fudgeAid.Node("Test", new fudge.Matrix4x4(), new fudge.Material("TestMtr", fudge.ShaderFlat, new fudge.CoatColored(fudge.Color.CSS("green"))), new fudge.MeshCube);
+        let testCamera = new fudge.ComponentCamera();
+        testCamera.pivot.translateZ(5);
+        testCamera.pivot.lookAt(fudge.Vector3.ZERO());
+        testCamera.backgroundColor = fudge.Color.CSS("White");
+        Platform_Edtior.editorViewport.initialize("Test", testNode, testCamera, editorCanvas);
+        Platform_Edtior.editorViewport.draw();
         Platform_Edtior.viewport.draw();
     }
     function pointerMove(_event) {
-        let scalar = 0.005;
-        if (!oldX || !oldY) {
-            oldX = _event.canvasX;
-            oldY = _event.canvasY;
-        }
-        else if (fudge.Keyboard.isPressedOne([fudge.KEYBOARD_CODE.SHIFT_LEFT])) {
-            let xChange = (_event.canvasX - oldX) * scalar;
-            let yChange = (_event.canvasY - oldY) * scalar;
+        let scale = 0.005;
+        if (fudge.Keyboard.isPressedOne([fudge.KEYBOARD_CODE.SHIFT_LEFT])) {
+            let xChange = (_event.canvasX - oldX) * scale;
+            let yChange = (_event.canvasY - oldY) * scale;
             Platform_Edtior.viewport.camera.pivot.translate(new fudge.Vector3(xChange, yChange, 0));
         }
         oldX = _event.canvasX;
         oldY = _event.canvasY;
         Platform_Edtior.viewport.draw();
+    }
+    function convertToMainViewport(selectedNode) {
+        Platform_Edtior.viewport.getGraph().removeChild(selectedNode);
+        Platform_Edtior.editorViewport.getGraph().addChild(selectedNode);
     }
 })(Platform_Edtior || (Platform_Edtior = {}));
 //# sourceMappingURL=crun.js.map
